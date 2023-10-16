ca998b27326f4746ae49942aec2ff27393012acd
Lisa <lisahongofficial@gmail.com>
2023-10-15 21:46:34 -0400

    add Scanner class to 21.

--- src/main/java/challenges/CarOps.java
+++ src/main/java/challenges/CarOps.java
@@ -5,10 +5,12 @@ import domain.Car;
 import java.util.ArrayList;
 import java.util.List;
 
+
 public class CarOps {
 
 //    21. Filter by Make: Filter the list of cars to only include cars with a specific make.
     public static List<Car> carListByMake(List<Car> cars, String make){
+
         return cars.stream()
                 .filter(s -> s.getMake().equalsIgnoreCase(make))
                 .toList();
--- src/main/java/data/Main.java
+++ src/main/java/data/Main.java
@@ -18,11 +18,17 @@ public class Main {
 //        FetchData.getStudentList().stream()
 //                .limit(5)
 //                .forEach(System.out::println);
-        List<Student> students = FetchData.getStudentList();
+//        List<Student> students = FetchData.getStudentList();
+////        14.
 //        System.out.println("Are all students are adults? " + StudentOps.allStudentsAdult(students));
-//        System.out.println("The oldest student is " + StudentOps.findOldestStudent(students));
-//        System.out.println("Convert first name to Uppercase: " + StudentOps.firstNameToUppercase(students));
 //
+////        15.
+//        System.out.println("The oldest student is " + StudentOps.findOldestStudent(students));
+//
+////        16.
+//        System.out.println("Convert first name to Uppercase: " + StudentOps.firstNameToUppercase(students));
+////
+////        17.
 //        System.out.print("Please enter an id to find a student: ");
 //        Scanner sc = new Scanner(System.in);
 //        int targetId = sc.nextInt();
@@ -32,12 +38,21 @@ public class Main {
 //            System.out.println("Student not found.");
 //        }
 //
+////        18.
 //        System.out.println("Students' count distributed by age: " + StudentOps.distributeByAge(students));
+//
+////        19.
 //        System.out.println("Students' group by age are: " + StudentOps.groupByAge(students));
+//
+////        20.
 //        System.out.println(StudentOps.deviationOfAge(students));
 
         List<Car> cars = FetchData.getCarList();
-        System.out.println(CarOps.carListByMake(cars, "BMW"));
+//21.
+        System.out.println("Please enter the car's make: ");
+        Scanner sc = new Scanner(System.in);
+        String carMake = sc.nextLine();
+        System.out.println("Car Lists sorted by " + carMake  + CarOps.carListByMake(cars, carMake));
     }
 
 }
